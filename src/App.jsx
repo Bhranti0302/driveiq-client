7ebcebf342ae38effefe0f54940dcbf73da597d8
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-08-20 21:24:49 +0530

    integrate application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import AppRoutes from "./routes/AppRoutes";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">DriveIQ Client</h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
