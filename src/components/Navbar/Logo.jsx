f7979363c16218392518ed6df7e5b635777204f0
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-08-22 21:06:53 +0530

    feat: add navbar logo component

---
+++ src/components/Navbar/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <a href="/">
+      <img
+        src="/src/assets/images/svg/Logo.svg"
+        alt="DriveIQ Logo"
+        className="h-10 w-auto"
+      />
+    </a>
+  );
+};
+
+export default Logo;
