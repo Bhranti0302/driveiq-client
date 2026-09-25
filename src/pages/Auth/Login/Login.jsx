6e9359a725a9f0b437a12830979be6a0a938613f
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-09-25 18:42:05 +0530

    feat: add login and signup pages

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,5 +1,32 @@
+import AuthPage from "../AuthPage";
+
+const loginFields = [
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "Type your email...",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "Type your password...",
+  },
+];
+
 const Login = () => {
-  return <h1>DriveIQ Login</h1>;
+  return (
+    <AuthPage
+      highlightText="Log In"
+      title="to your Account"
+      fields={loginFields}
+      buttonText="Register"
+      linkQuestion="Already have an account?"
+      linkText="Sign In"
+      linkTo="/signup"
+    />
+  );
 };
 
 export default Login;
--- src/pages/Auth/Signup/Signup.jsx
+++ src/pages/Auth/Signup/Signup.jsx
@@ -1,5 +1,50 @@
+import AuthPage from "../AuthPage";
+
+const signupFields = [
+  {
+    name: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "Type your name...",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "Type your email...",
+  },
+  {
+    name: "phone",
+    label: "Phone No.",
+    type: "tel",
+    placeholder: "Type your phone number...",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "Type your password...",
+  },
+  {
+    name: "confirmPassword",
+    label: "Confirm Password",
+    type: "password",
+    placeholder: "Type your password again...",
+  },
+];
+
 const Signup = () => {
-  return <h1>DriveIQ Signup</h1>;
+  return (
+    <AuthPage
+      highlightText="Create"
+      title="Your Account"
+      fields={signupFields}
+      buttonText="Register"
+      linkQuestion="Already have an account?"
+      linkText="Login"
+      linkTo="/login"
+    />
+  );
 };
 
 export default Signup;
